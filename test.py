f92aad3afa99b5244a1f2ca31e18b144f9bb810f
DESKTOP-GE06ASF\sina_ <buffy-jeanne@hotmail.de>
2021-01-08 15:14:02 +0100

    change1

--- test.py
+++ test.py
@@ -1,3 +1,67 @@
-import pandas
-1+1
-s
+a=3  #assignment
+print(a)
+b=a**2 #exponential
+print(b)
+c=10%3 #modulo: in division what is left (10%3=9 rest 1)
+print(c)
+d=10//3 #division with whole numbers
+print(d)
+e=10/2 #division
+print(e)
+
+
+
+#if 1<2:
+ #   print("Welcome")
+a=3
+if  2==2:
+	a=4
+	print(a)
+
+
+
+if not True:
+	print("1")
+elif not (1+1==3):
+	print("2")
+else: 
+	print("3")
+print(1)
+
+
+
+
+#
+x = 4
+y = 2
+if  1 + 1 == y or x == 4 and 7 == 8: #=1 + 1 == y or (x == 4 and 7 == 8) -> and is before or
+  print("Yes")
+elif x > y:
+  print("No")
+
+#booleans
+print(1==1)
+print(1==1 and 1==2)
+print(1==1 or 1==2)
+print(not 1==1)
+print(not 1==1 or  1==2)
+print(False== False)
+print(False== (False or True))
+
+#lists
+words=["Hello", "world", "!"]
+print(words[0])
+
+empty_list=[]
+print(empty_list)
+
+#lists can contain several different types
+#lists can contain several lists
+number=3
+things =["string", 0, [1,2,number], 4,56]
+print(things[1])
+print(things[2])
+
+
+
+
